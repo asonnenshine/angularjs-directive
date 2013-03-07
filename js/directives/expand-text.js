67a2a67430859b201ed31536d81a1144fb83e835
Kip Armstrong <kip.armstrong@gmail.com>
2013-03-07 02:29:45 -0700

    Added basic truncation

--- js/directives/expand-text.js
+++ js/directives/expand-text.js
@@ -1,8 +1,24 @@
-// NEW: Define your own directive (See http://docs.angularjs.org/guide/directive )
 angular.module("answer", []).directive("expandText", function() {
+	// NEW: Truncation function
+	function truncateWordBounds(full, len) {
+		var last = full.substr(len, 1);
+		var abbrev = full.substr(0, len);
+		if (RegExp(/[A-Za-z]/).test(last)) {
+		abbrev = abbrev.replace(/[A-Za-z]+$/, '');
+		}
+		return abbrev;
+	}
+
 	return {
 		restrict: "A",
-		template: "<p><i>{{text}}</i></p>",
-		replace: true
+		template: "<p>{{text}}</p>",
+		replace: true,
+		// NEW: Add linking function
+		link: function(scope, element, attributes) {
+			var maxLength = 100;
+			if(scope.text.length > maxLength) {
+				scope.text = truncateWordBounds(scope.text, maxLength) + "...";
+			}
+		}
 	}
 });
