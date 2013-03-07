f231f0253296e5c36781575fd824d3f5163d8ec5
Kip Armstrong <kip.armstrong@gmail.com>
2013-03-07 02:42:20 -0700

    Added maxlength config param

--- js/directives/expand-text.js
+++ js/directives/expand-text.js
@@ -1,5 +1,4 @@
 angular.module("answer", []).directive("expandText", function() {
-	// NEW: Truncation function
 	function truncateWordBounds(full, len) {
 		var last = full.substr(len, 1);
 		var abbrev = full.substr(0, len);
@@ -13,9 +12,9 @@ angular.module("answer", []).directive("expandText", function() {
 		restrict: "A",
 		template: "<p>{{text}}</p>",
 		replace: true,
-		// NEW: Add linking function
 		link: function(scope, element, attributes) {
-			var maxLength = 100;
+			// NEW: Get maxlength from attributes param
+			var maxLength = attributes["maxLength"] || 100;
 			if(scope.text.length > maxLength) {
 				scope.text = truncateWordBounds(scope.text, maxLength) + "...";
 			}
