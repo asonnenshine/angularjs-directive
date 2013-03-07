ec63da67bf1c7c2d64010f27e8341fb1e824bb9d
Kip Armstrong <kip.armstrong@gmail.com>
2013-03-07 02:53:56 -0700

    Added show more/show less links

--- js/directives/expand-text.js
+++ js/directives/expand-text.js
@@ -5,18 +5,26 @@ angular.module("answer", []).directive("expandText", function() {
 		if (RegExp(/[A-Za-z]/).test(last)) {
 		abbrev = abbrev.replace(/[A-Za-z]+$/, '');
 		}
-		return abbrev;
+		// NEW: Return the part that got chopped as well
+		return {truncated : abbrev, expanded: full.substr(abbrev.length)};
 	}
 
 	return {
 		restrict: "A",
-		template: "<p>{{text}}</p>",
+		// NEW: Update template with show more and show less links for toggling back and forth
+		template: "<p><span>{{text}}</span><span data-ng-show='truncated'>...</span><span data-ng-hide='truncated'>{{expandedText}}</span><a href='' data-ng-show='truncated' data-ng-click='truncated=false'>Show More</a><a href='' data-ng-hide='truncated || disabled' data-ng-click='truncated=true'>Show Less</a></p>",
 		replace: true,
 		link: function(scope, element, attributes) {
-			// NEW: Get maxlength from attributes param
 			var maxLength = attributes["maxLength"] || 100;
+			// NEW: Add both halves of truncated text to scope
 			if(scope.text.length > maxLength) {
-				scope.text = truncateWordBounds(scope.text, maxLength) + "...";
+				scope.truncated = true;
+				var splitText = truncateWordBounds(scope.text, maxLength);
+				scope.text = splitText.truncated;
+				scope.expandedText = splitText.expanded;
+			}
+			else {
+				scope.disabled = true; // no need for truncation
 			}
 		}
 	}
